fdab83a473158797b0a6b0d2b35db653a0172e24
alinaparonyan <48366768+alinaparonyan@users.noreply.github.com>
2021-12-19 12:46:45 +0300

    Update scrna_normalization.py

--- scrna_normalization.py
+++ scrna_normalization.py
@@ -10,21 +10,15 @@ def LogNormalize(data: ad.AnnData, scale_factor: int):
 
     Args:
         data (ad.AnnData): Dataset
-        scale_factor (int): Sets the scale factor for cell-level normalization.
     Returns:
         ad.AnnData: Normalized dataset with normalized counts in data.X
         and non-normalized counts in data.layer["counts"].
     """
 
-    # Recalculate count sum for each cell as gene QC filters could be applied
-    count_sum = np.sum(data.X, axis=1)
-    data.obs["n_counts_recalc"] = np.asarray(count_sum).reshape(-1)
-
     # Save non-normalized counts in separate layer
     data.layers["counts"] = data.X.copy()
 
-    # Normalize & save normalized counts in data.X
-    data.X /= data.obs["n_counts_recalc"].values[:, None]
-    data.X *= scale_factor
+    # Normalize & Log-transform
+    data.X /= data.obs['size_factors'].values[:,None]
     data.X = sc.pp.log1p(data.X)
     return data
